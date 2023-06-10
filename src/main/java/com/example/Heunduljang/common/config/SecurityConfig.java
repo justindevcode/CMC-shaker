0aa602d45aca5d914a372df77661d41dadb5e2ab
HwaSup <ghktjq1119@naver.com>
2023-06-10 23:02:49 +0900

    feat : 시큐리티 설정 구현

--- src/main/java/com/example/Heunduljang/common/config/SecurityConfig.java
+++ src/main/java/com/example/Heunduljang/common/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.example.Heunduljang.common.config;
 
 
+import com.example.Heunduljang.common.filter.AuthenticationFilter;
+import com.example.Heunduljang.common.filter.ExceptionFilter;
+import com.example.Heunduljang.user.service.CustomUserDetailService;
+import com.example.Heunduljang.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -16,6 +19,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final ExceptionFilter exceptionFilter;
+    private final CustomUserDetailService customUserDetailService;
+    private final UserService userService;
+
+    @Override
+    public void configure(WebSecurity web)  {
+        web.ignoring().antMatchers("/v3/api-docs", "/swagger*/**" );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,10 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 세션을 사용하지 않기 때문에 STATELESS 로 설정
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(new AuthenticationFilter(authenticationManager(),customUserDetailService,userService))
                 .authorizeRequests()
-//                .antMatchers("/login/apple").permitAll()
-                .antMatchers("**").permitAll()
-//                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers("/swagger*/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(exceptionFilter, AuthenticationFilter.class);
+
     }
 }
