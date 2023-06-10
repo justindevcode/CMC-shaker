28558c9434742ce221cdf478453e3c130355807b
HwaSup <ghktjq1119@naver.com>
2023-06-10 14:41:41 +0900

    feat : 환경설정 세팅

---
+++ src/test/java/com/example/Heunduljang/HeunduljangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Heunduljang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeunduljangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
